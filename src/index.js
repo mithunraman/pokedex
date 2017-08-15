079cd23c7489b6b6bbfe9571aeb29022502d63eb
mithun <mithun@mithun-master.tailian.com>
2017-08-15 18:48:49 +0530

    updated history

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
-import {Router, browserHistory} from 'react-router';
+import {Router, browserHistory, hashHistory} from 'react-router';
 import promise from 'redux-promise';
 
 import reducers from './reducers';
@@ -12,6 +12,6 @@ const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
-        <Router history={browserHistory} routes={routes}/>
+        <Router history={hashHistory} routes={routes}/>
     </Provider>
     , document.querySelector('.container'));
